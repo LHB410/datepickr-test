e8b5b4af814fd540e2b8ace7ffbc2f4fa2a65c47
Laura Brooks <laura.brooks0@outlook.com>
2023-01-26 13:44:13 +0900

    initial commit

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
+import "flatpickr/dist/themes/dark.css";
+
+
+
 
 export default class extends Controller {
+  static targets = [ "inputDate" ]
+
   connect() {
-    this.element.textContent = "Hello World!"
+    flatpickr(this.inputDateTarget, {
+      altInput: true,
+      altFormat: "F j, Y",
+      dateFormat: "Y-m-d",
+      min_date: Time.zone.now,
+      enableTime: true,
+    });
   }
 }
